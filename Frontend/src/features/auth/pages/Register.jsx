427a4cb00b7d2fac4279922ce50548babc22b9a9
Sumit yadav <sumitkumaryadav9608700@gmail.com>
2026-04-17 23:53:23 +0530

    hello

---
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <main>
+      <div className="form-contener">
+        <form >
+          
+        </form>
+      </div>
+    </main>
+  )
+}
+
+export default Register
