a27faea2c214ac3f09e7605c42f1e213cb51221a
Sumit yadav <sumitkumaryadav9608700@gmail.com>
2026-04-24 13:43:18 +0530

    complted auth configerstion

--- Backend/src/controller/song.controller.js
+++ Backend/src/controller/song.controller.js
@@ -1,1 +1,14 @@
 const  songModel = require("../models/song.models");
+const  id3 = require("node-id3");
+
+
+
+async function uploadSong(req, res) {
+  const tags = id3.read(req.file.buffer);
+  console.log(tags)
+  
+}
+
+
+
+module.exports = {uploadSong};
--- Backend/src/routes/song.routes.js
+++ Backend/src/routes/song.routes.js
@@ -1,12 +1,13 @@
 const {Router} = require("express");
 const upload =  require("../middleware/song.middleware");
+const songController = require("../controller/song.controller");
 
 
 const routes =  Router();
 
 
 
-routes.post("/",upload.single("song"),)
+routes.post("/",upload.single("song"),songController.uploadSong);
 
 
 module.exports = routes;
