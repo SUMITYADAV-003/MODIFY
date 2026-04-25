85a4336b9ea5947bf65be8b58f81739f5ec804ad
Sumit yadav <sumitkumaryadav9608700@gmail.com>
2026-04-25 20:47:08 +0530

    complete song models

--- Backend/src/controller/song.controller.js
+++ Backend/src/controller/song.controller.js
@@ -1,12 +1,28 @@
 const  songModel = require("../models/song.models");
+const storageService = require("../service/Storage.service")
 const  id3 = require("node-id3");
 
 
 
 async function uploadSong(req, res) {
-  const tags = id3.read(req.file.buffer);
-  console.log(tags)
-  
+  const songBuffer = req.file.songBuffer
+  const {mood} = req.body;
+  const tags = id3.read(songBuffer);
+
+  const [songFile, posterFile] = await Promise.all([
+    storageService.uploadeFile({
+      buffer: songBuffer,
+      filename: tags.title + ".mp3",
+      folder: "/cohort-2/moodify/songs"
+    }),
+    storageService.uploadeFile({
+      buffer: tags.image.imageBuffer,
+      filename: tags.title + ".mp3",
+      folder: "/cohort-2/moodify/posters"
+    })
+  ])
+
+
 }
 
 
