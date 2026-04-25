910c50bbe9580bdae50b3585ffe5af483792defa
Sumit yadav <sumitkumaryadav9608700@gmail.com>
2026-04-25 20:16:21 +0530

    complete song models

---
+++ Backend/src/service/Storage.service.js
@@ -0,0 +1,20 @@
+const ImageKit = require("@imagekit/nodejs").default
+
+const client = new ImageKit({
+  privateKey: process.env.IMAGE_KIT_PRIVATEKEY,
+})
+
+async function uploadeFile({buffer, filename, folder = ""}) {
+
+  const file = await client.files.upload({
+    file: await ImageKit.toFile(Buffer.from(buffer)),
+    fileName: filename,
+    folder
+
+  })
+  return file
+  
+}
+
+
+module.exports = {uploadeFile};
