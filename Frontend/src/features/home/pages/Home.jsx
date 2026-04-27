c2e6f9876d1561b5e52627711b94921f7c9b4e1c
Sumit yadav <sumitkumaryadav9608700@gmail.com>
2026-04-27 19:52:41 +0530

    Home routes

--- Frontend/src/features/home/pages/Home.jsx
+++ Frontend/src/features/home/pages/Home.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import FaceExpression from '../../Expression/compontent/FaceExpression';
 
 const Home = () => {
   return (
     <div>
-      Home 
+     <FaceExpression />
     </div>
   )
 }
